1a7f4375cffe48cb7c60360fab3e04f2d9792b2e
kart1ka <kartik8942@gmail.com>
2021-10-01 18:55:38 +0530

    Working with _getStaticPaths and fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -14,21 +14,40 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
   // fetch data for a single meetup
 
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
+  console.log(meetupId); // is not logged in the browser's console but in the integrated terminal in the vscode because this code runs on the server and not on the client side
 
   return {
     props: {
       meetupData: {
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1200px-Stadtbild_M%C3%BCnchen.jpg",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1200px-Stadtbild_M%C3%BCnchen.jpg",
         id: meetupId,
         title: "A First Meetup",
-        address="Some Street 5, Some City",
-        description="This is a first meetup!"
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup!",
       },
     },
   };
