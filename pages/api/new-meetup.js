73cc27ffe886f5edb9f21f062beb8f2e1dbfc84f
kart1ka <kartik8942@gmail.com>
2021-10-02 12:34:10 +0530

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,24 @@
+import { MongoClient } from "mongodb";
+
 // api/new-meetup
 // POST  /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://kart1k:kart1ka1shu@cluster0.umz0q.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
