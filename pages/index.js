978dac713476f347b9b596ac508029cdfbbe6c9b
kart1ka <kartik8942@gmail.com>
2021-10-04 21:32:52 +0530

    Get data from the database

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
+
 // import Layout from "../components/layout/Layout";
 const DUMMY_MEETUPS = [
   {
@@ -40,12 +42,74 @@ function HomePage(props) {
 // Static Generation
 export async function getStaticProps() {
   // fetch data from an API
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://kart1k:EzDblojaEonzWN34@cluster0.umz0q.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10, // Incremental Static Generation
   };
 }
 
 export default HomePage;
+
+// import { MongoClient } from "mongodb";
+// import MeetupList from "../components/meetups/MeetupList";
+
+// function HomePage(props) {
+//   return <MeetupList meetups={props.meetups} />;
+// }
+
+// // export async function getServerSideProps(context) {
+// //   const req = context.req;
+// //   const res = context.res;
+
+// //   // fetch data from an API
+
+// //   return {
+// //     props: {
+// //       meetups: DUMMY_MEETUPS
+// //     }
+// //   };
+// // }
+
+// export async function getStaticProps() {
+//   // fetch data from an API
+//   const client = await MongoClient.connect(
+//     "mongodb+srv://kart1k:EzDblojaEonzWN34@cluster0.umz0q.mongodb.net/meetups?retryWrites=true&w=majority"
+//   );
+//   const db = client.db();
+
+//   const meetupsCollection = db.collection("meetups");
+
+//   const meetups = await meetupsCollection.find().toArray();
+
+//   client.close();
+
+//   return {
+//     props: {
+//       meetups: meetups.map((meetup) => ({
+//         title: meetup.title,
+//         address: meetup.address,
+//         image: meetup.image,
+//         id: meetup._id.toString(),
+//       })),
+//     },
+//     revalidate: 1,
+//   };
+// }
+
+// export default HomePage;
